dfb75622dc11a6cc1549f80aa88507b241ea6eb3
PedidosClear <62444106+PedidosClear@users.noreply.github.com>
2020-06-30 16:19:49 -0300

    asd

--- api/notifications/index.js
+++ api/notifications/index.js
@@ -4,46 +4,27 @@ var dotenv = require('dotenv');
 const fetch = require('node-fetch');
 
 dotenv.config();
+
+global.buscar = ''
  
-router.post('/create', async(req, res)=>{
-
-  const external_id = req.query.external_id
-  const body = req.body.json;
-
-  fetch('https://api.mercadopago.com/mpmobile/instore/qr/'+process.env.COLLECTOR_ID+'/'+external_id+'?access_token='+process.env.ACCESS_TOKEN, {
-        method: 'post',
-        body:    body,
-        headers: { 'Content-Type': 'application/json' }
-    })
-    .then(r => r.json())
-    .then(json => res.status(200).json(json));
-})
-
 router.get('/get', async(req, res)=>{
 
-    // const body = req.query.external_reference;
-  
-    // fetch('https://api.mercadopago.com/pos?access_token='+process.env.ACCESS_TOKEN+'&external_id='+body)
-    //   .then(r => r.json())
-    //   .then(json => res.status(200).json(json));
-    res.status(200)
-  })
-
-router.get('/status', async(req, res)=>{
-
-    const body = req.query.external_reference;
-  
-    fetch('https://api.mercadopago.com/merchant_orders/search?external_reference='+body+'&access_token='+process.env.ACCESS_TOKEN)
-      .then(r => r.json())
-      .then(json => res.status(200).json(json));
+     if(global.buscar !=''){
+       fetch(global.buscar+'?access_token='+process.env.ACCESS_TOKEN)
+         .then(r => r.json())
+         .then(json => res.status(200).json(json));
+     }
+    res.status(200).json('OK')
   })
 
   router.post('/notifications', async(req, res)=>{
   
+    console.log('Notificacion'); 
+    console.log(req.body);;
     const body = req.body;
-    console.log(body)
-    console.log(body.json)
-    console.log(req.query);
+    if(body.topic == 'payment'){
+      global.buscar = body.resource
+    }
     res.status(200).json('OK')
 })
 
