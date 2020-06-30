caa13453d40b40fd5890bcf0b81f91f8b6490276
PedidosClear <62444106+PedidosClear@users.noreply.github.com>
2020-06-30 04:09:34 -0300

    ads

--- public/assets/js/acme.js
+++ public/assets/js/acme.js
@@ -75,7 +75,7 @@ $(document).ready(function() {
 					// para recibir las notificaciones en tu endpoint público.
 
 					var orderJSON ={"external_reference": external_reference,
-									"notification_url": "",
+									"notification_url": "https://matimaldo-mercadopago-qr.herokuapp.com/api/notifications/notifications",
 									"items" : items
 									};
 
